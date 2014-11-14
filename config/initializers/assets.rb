d257b8a1d6bbb7a21ef0d7974bc8af39acd095a0
Chris Salguero <cfsalguero93@gmail.com>
2014-11-13 18:45:09 -0800

    Extremely basic css displaying, no plugin supported yet.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( style.css ) #Added when trying to make CSS work
+Rails.application.config.assets.precompile += %w( theme-colors/default.css )
